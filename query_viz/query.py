2c748810370268344c4d7cf44582ffb202315588
Federico Razzoli <federico.razzoli@vettabase.com>
2025-08-09 11:26:11 +0100

    Obsolete QueryConfig.column, introduce columns list

--- query_viz/query.py
+++ query_viz/query.py
@@ -10,7 +10,12 @@ class QueryConfig:
         self.name = config['name']
         self.query = config['query']
         self.connection_name = config.get('connection', default_connection)
-        self.column = config.get('column')
+        self.column = config.get('column') # OBSOLETE
         self.interval = config.get('interval', global_interval)
         self.description = config.get('description')
         self.color = config.get('color')
+
+        if self.column:
+            self.columns = ['time', self.column]
+        else:
+            self.columns = ['time', 'value']
