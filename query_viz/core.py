8c7dead19098e0462ccdc05feea231ed91d91133
Federico Razzoli <federico.razzoli@vettabase.com>
2025-08-22 20:00:02 +0100

    Validate global global_keep_history with the new method

--- query_viz/core.py
+++ query_viz/core.py
@@ -131,8 +131,8 @@ class QueryViz:
             raise QueryVizError("Global 'on_rotation_keep_datapoints' must be an integer. Minimum value: {MIN_ON_ROTATION_KEEP_DATAPOINTS}")
         
         # Validate global on_file_rotation_keep_history
-        if 'on_file_rotation_keep_history' not in self.config:
-            raise QueryVizError("Global 'on_file_rotation_keep_history' is required")
+        #if 'on_file_rotation_keep_history' not in self.config:
+        #    raise QueryVizError("Global 'on_file_rotation_keep_history' is required")
         
         query_names = set()
         for i, query in enumerate(queries):
@@ -335,6 +335,8 @@ class QueryViz:
         global_interval = self.config['interval']
         global_keep_datapoints = self.config['on_rotation_keep_datapoints']
         global_keep_history = self.config['on_file_rotation_keep_history']
+        
+        QueryConfig.set_global_interval('on_file_rotation_keep_history', global_keep_history)
         
         for i, query_config in enumerate(self.config['queries']):
             if 'on_rotation_keep_datapoints' not in query_config:
