0bd3fb3dd8f4ece3e66bd7de86e025a2b3f6756c
Federico Razzoli <federico.razzoli@vettabase.com>
2025-09-01 03:07:05 +0100

    Add MariaDBConnectoin.info

--- query_viz/database/mariadb.py
+++ query_viz/database/mariadb.py
@@ -9,6 +9,15 @@ from ..exceptions import QueryVizError
 
 class MariaDBConnection(DatabaseConnection):
     """MariaDB database connection, with connection pooling support"""
+    info = {
+        "connector-name": "QV-MariaDB",
+        "connector-url": "https://github.com/Vettabase/query-viz",
+        "version": "0.1",
+        "maturity": "gamma",
+        "license": "AGPLv3",
+        "copyright": "2025, Vettabase Ltd",
+        "authors": [{"name": "Vettabase Ltd", "url": "https://vettabase.com"}]
+    }
     
     def __init__(self, config, db_timeout):
         super().__init__(config, db_timeout)
