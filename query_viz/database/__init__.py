eec0cccd5bb8e1c78c38d15becc8ec8d313b308c
Federico Razzoli <federico.razzoli@vettabase.com>
2025-09-03 03:00:48 +0100

    Add MySQL support

--- query_viz/database/__init__.py
+++ query_viz/database/__init__.py
@@ -4,5 +4,14 @@ Database connections package
 
 from .base import DatabaseConnection, SUCCESS, FAIL
 from .mariadb import MariaDBConnection
+from .mysql import MySQLConnection
 
-__all__ = ['DatabaseConnection', 'MariaDBConnection', 'SUCCESS', 'FAIL']
+__all__ = [
+    # Generic requirements
+    'DatabaseConnection',
+    'SUCCESS',
+    'FAIL',
+    # Connectors
+    'MariaDBConnection',
+    'MySQLConnection'
+]
